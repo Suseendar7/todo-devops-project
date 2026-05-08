219755fde8cf27ceaa93a10cdb16daa1acadd07a
suseendar7 <suseendar7@gmail.com>
2026-05-08 09:35:01 +0530

    Updated Version

--- backend/server.py
+++ backend/server.py
@@ -1,37 +1,102 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import subprocess
+import shutil
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
 
 client = MongoClient("mongodb://mongo:27017/")
-db = client["todo_db"]
-collection = db["tasks"]
+db = client["devops_db"]
+collection = db["builds"]
+
+REPO_DIR = "repo"
 
 @app.route("/")
 def home():
-    return "Backend Updated!"
+    return {
+        "service": "DevOps Build System",
+        "status": "running",
+        "version": "1.0"
+    }
 
-@app.route("/tasks", methods=["GET"])
-def get_tasks():
-    tasks = []
-    for t in collection.find():
-        tasks.append(t["task"])
-    return jsonify(tasks)
+@app.route("/build", methods=["POST"])
+def build_repo():
 
-@app.route("/tasks", methods=["POST"])
-def add_task():
     data = request.json
+    repo_url = data.get("repo")
 
-    if not data or "task" not in data:
-        return jsonify({"error": "Task is required"}), 400
+    if not repo_url:
+        return jsonify({"error": "Repository URL required"}), 400
+
+    # Delete old repo folder
+    if os.path.exists(REPO_DIR):
+        shutil.rmtree(REPO_DIR)
+
+    logs = ""
 
     try:
-        collection.insert_one({"task": data["task"]})
-        return jsonify({"message": "Task added"})
-    except:
-        return jsonify({"error": "Failed to add task"}), 500
+
+        # Clone repo
+        clone = subprocess.run(
+            ["git", "clone", repo_url, REPO_DIR],
+            capture_output=True,
+            text=True
+        )
+
+        logs += clone.stdout + clone.stderr
+
+        if clone.returncode != 0:
+            raise Exception("Clone failed")
+
+        # Build docker image
+        build = subprocess.run(
+            ["docker", "build", "-t", "ci-build", REPO_DIR],
+            capture_output=True,
+            text=True
+        )
+
+        logs += build.stdout + build.stderr
+
+        if build.returncode == 0:
+            status = "SUCCESS"
+        else:
+            status = "FAILED"
+
+    except Exception as e:
+        status = "FAILED"
+        logs += str(e)
+
+    # Save build history
+    collection.insert_one({
+        "repo": repo_url,
+        "status": status,
+        "logs": logs,
+        "time": str(datetime.now())
+    })
+
+    return jsonify({
+        "status": status,
+        "logs": logs[-1000:]
+    })
+
+@app.route("/history", methods=["GET"])
+def history():
+
+    builds = []
+
+    for b in collection.find().sort("_id", -1):
+
+        builds.append({
+            "repo": b["repo"],
+            "status": b["status"],
+            "time": b["time"]
+        })
+
+    return jsonify(builds)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
